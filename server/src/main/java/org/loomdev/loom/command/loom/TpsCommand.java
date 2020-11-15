3f0db979f1333fd948b09af1cade884d45ae2c8d
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2020-11-15 14:08:31 -0800

    Use Components for TPS command

--- server/src/main/java/org/loomdev/loom/command/loom/TpsCommand.java
+++ server/src/main/java/org/loomdev/loom/command/loom/TpsCommand.java
@@ -1,26 +1,23 @@
 package org.loomdev.loom.command.loom;
 
-import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.TextColor;
 import org.jetbrains.annotations.NotNull;
 import org.loomdev.api.command.Command;
 import org.loomdev.api.command.CommandContext;
-import org.loomdev.api.command.CommandSource;
 import org.loomdev.api.monitoring.TickTimes;
 import org.loomdev.api.monitoring.Tps;
 import org.loomdev.api.server.Server;
+import org.loomdev.api.util.ChatColor;
 import org.loomdev.loom.server.ServerImpl;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class TpsCommand extends Command {
 
-    private final Server server;
-
     private static final DecimalFormat FORMAT = new DecimalFormat("########0.0");
+    private final Server server;
 
     public TpsCommand(ServerImpl server) {
         super("tps", "mspt");
@@ -32,53 +29,60 @@ public class TpsCommand extends Command {
 
     @Override
     public void execute(@NotNull CommandContext context) {
-        Tps tps = server.getTps();
-        TickTimes tickTimes = server.getTickTimes();
+        var tps = server.getTps();
+        var tickTimes = server.getTickTimes();
+        var comma = Component.text(", ");
 
-        List<String> times = new ArrayList<>();
-        times.addAll(eval(tickTimes.getTimes(TickTimes.TickTimesInterval.SECONDS_5)));
-        times.addAll(eval(tickTimes.getTimes(TickTimes.TickTimesInterval.SECONDS_10)));
-        times.addAll(eval(tickTimes.getTimes(TickTimes.TickTimesInterval.SECONDS_60)));
+        var ticks = Component.text("TPS from the last 1m, 5m, 15m: ")
+                .append(formatTps(tps, Tps.TpsInterval.MINUTES_1))
+                .append(comma)
+                .append(formatTps(tps, Tps.TpsInterval.MINUTES_5))
+                .append(comma)
+                .append(formatTps(tps, Tps.TpsInterval.MINUTES_15));
 
-        context.getSource().sendMessage(TextComponent.builder()
-                .append(TextComponent.of("TPS from the last 1m, 5m, 15m: "))
-                .append(TextComponent.of(String.format("%s§f, %s§f, %s",
-                        getFormattedTps(tps, Tps.TpsInterval.MINUTES_1),
-                        getFormattedTps(tps, Tps.TpsInterval.MINUTES_5),
-                        getFormattedTps(tps, Tps.TpsInterval.MINUTES_15))))
-                .build()
-        );
+        var ticks1 = Component.text("Tick rates: ")
+                .append(formatTickRates(tickTimes.getTimes(TickTimes.TickTimesInterval.SECONDS_5)))
+                .append(comma)
+                .append(formatTickRates(tickTimes.getTimes(TickTimes.TickTimesInterval.SECONDS_10)))
+                .append(comma)
+                .append(formatTickRates(tickTimes.getTimes(TickTimes.TickTimesInterval.SECONDS_60)))
+                .hoverEvent(HoverEvent.showText(Component.text("Tick rates from last 5s, 10s, 15s")));
 
-        context.getSource().sendMessage(TextComponent.builder()
-                .append(TextComponent.of("Tick rates: "))
-                .append(TextComponent.of(String.format("%s§7/%s§7/%s§f, %s§7/%s§7/%s§f, %s§7/%s§7/%s", times.toArray()))
-                        .hoverEvent(HoverEvent.showText(TextComponent.of("Tick rates from last 5s, 10s, 15s"))))
-                .build()
-        );
+        context.getSource().sendMessage(ticks.append(Component.newline()).append(ticks1));
     }
 
-    private static String getFormattedTps(Tps tps, Tps.TpsInterval interval) {
-        double tpsValue = tps.getTps(interval);
-        return ((tpsValue > 18.0) ? "§a" : (tpsValue > 16.0) ? "§e" : "§c") + ((tpsValue > 21.0) ? "*" : "")
-                + Math.min(Math.round(tpsValue * 100.0) / 100.0, 20.0);
+    private Component formatTps(Tps tps, Tps.TpsInterval interval) {
+        var value = tps.getTps(interval);
+        var color = value > 18.0 ? ChatColor.GREEN : value > 16.0 ? ChatColor.YELLOW : ChatColor.RED;
+        return Component.text(Math.min(Math.round(value * 100.0) / 100.0, 20.0))
+                .append((value > 21.0) ? Component.text("*") : Component.empty())
+                .color(color);
     }
 
-    private static List<String> eval(long[] times) {
+    private Component formatTickRates(long[] times) {
         long min = Integer.MAX_VALUE;
         long max = 0L;
         long total = 0L;
+
         for (long value : times) {
             if (value > 0L && value < min) min = value;
             if (value > max) max = value;
             total += value;
         }
+
         double avgD = ((double) total / (double) times.length) * 1.0E-6D;
         double minD = ((double) min) * 1.0E-6D;
         double maxD = ((double) max) * 1.0E-6D;
-        return Arrays.asList(getColor(avgD), getColor(minD), getColor(maxD));
+
+        var separator = Component.text("/").color(ChatColor.WHITE);
+        return Component.text(FORMAT.format(avgD)).color(getColor(avgD))
+                .append(separator)
+                .append(Component.text(FORMAT.format(minD)).color(getColor(minD)))
+                .append(separator)
+                .append(Component.text(FORMAT.format(maxD)).color(getColor(maxD)));
     }
 
-    private static String getColor(double avg) {
-        return "§" + (avg >= 50 ? "c" : avg >= 40 ? "e" : "a") + FORMAT.format(avg);
+    private TextColor getColor(double time) {
+        return time >= 50 ? ChatColor.RED : time >= 40 ? ChatColor.YELLOW : ChatColor.GREEN;
     }
 }
