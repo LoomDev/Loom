a9c02bbe200ed52421101208846c247e69091280
Jeroen Verwimp <jeroen@verwimp.me>
2020-11-21 18:57:16 +0100

    Fixed ApiVersion

--- api/src/main/java/org/loomdev/api/ApiVersion.java
+++ api/src/main/java/org/loomdev/api/ApiVersion.java
@@ -1,46 +1,57 @@
 package org.loomdev.api;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public enum ApiVersion {
 
-    UNKNOWN("unknown", false),
-    v1_16_1R1("1.16.1-R1", true),
-    v1_16_2R1("1.16.2-R1", true),
-    v1_16_3RC1("1.16.3-RC1", false),
-    v1_16_3R1("1.16.3-R1", true),
-    v1_16_4PRE1("1.16.4-PRE1", false),
-    v1_16_4PRE2("1.16.4-PRE2", false),
-    v1_16_4RC1("1.16.4-RC1", false),
-    v1_16_4R1("1.16.4", true),
-    v20W45A("20w45a", false),
-    v20W46A("20w46a", false);
+    UNKNOWN("unknown", ReleaseType.UNKNOWN),
+    v1_16_1R1("1.16.1-R1", ReleaseType.RELEASE),
+    v1_16_2R1("1.16.2-R1", ReleaseType.RELEASE),
+    v1_16_3RC1("1.16.3-RC1", ReleaseType.RELEASE_CANDIDATE),
+    v1_16_3R1("1.16.3-R1", ReleaseType.RELEASE),
+    v1_16_4PRE1("1.16.4-PRE1", ReleaseType.PRE_RELEASE),
+    v1_16_4PRE2("1.16.4-PRE2", ReleaseType.PRE_RELEASE),
+    v1_16_4RC1("1.16.4-RC1", ReleaseType.RELEASE_CANDIDATE),
+    v1_16_4R1("1.16.4", ReleaseType.RELEASE),
+    v20W45A("20w45a", ReleaseType.SNAPSHOT),
+    v20W46A("20w46a", ReleaseType.SNAPSHOT);
 
     public static final ApiVersion LATEST = values()[ApiVersion.values().length - 1];
-    public static final ApiVersion LATEST_RELEASE = Arrays.stream(values())
-            .filter(v -> !v.isRelease())
-            .reduce((a, b) -> b).get();
-    public static final ApiVersion LATEST_SNAPSHOT = Arrays.stream(values())
-            .filter(ApiVersion::isRelease)
-            .reduce((a, b) -> b).get();
+    public static final ApiVersion LATEST_SNAPSHOT = getLatestByType(ReleaseType.SNAPSHOT);
+    public static final ApiVersion LATEST_PRE_RELEASE = getLatestByType(ReleaseType.PRE_RELEASE);
+    public static final ApiVersion LATEST_RELEASE_CANDIDATE = getLatestByType(ReleaseType.RELEASE_CANDIDATE);
+    public static final ApiVersion LATEST_RELEASE = getLatestByType(ReleaseType.RELEASE);
 
     private static final Map<String, ApiVersion> NAME_VERSION_MAP = Arrays.stream(values()).collect(Collectors.toMap(ApiVersion::getName, apiVersion -> apiVersion));
     private final String name;
-    private final boolean release;
+    private final ReleaseType releaseType;
 
-    ApiVersion(String name, boolean release) {
+    ApiVersion(String name, ReleaseType releaseType) {
         this.name = name;
-        this.release = release;
+        this.releaseType = releaseType;
     }
 
     public String getName() {
         return name;
     }
 
+    public boolean isSnapshot() {
+        return releaseType == ReleaseType.SNAPSHOT;
+    }
+
+    public boolean isPreRelease() {
+        return releaseType == ReleaseType.PRE_RELEASE;
+    }
+
+    public boolean isReleaseCandidate() {
+        return releaseType == ReleaseType.RELEASE_CANDIDATE;
+    }
+
     public boolean isRelease() {
-        return release;
+        return releaseType == ReleaseType.RELEASE;
     }
 
     public boolean isLatest() {
@@ -67,10 +78,20 @@ public enum ApiVersion {
         return NAME_VERSION_MAP.get(name);
     }
 
+    private static ApiVersion getLatestByType(ReleaseType type) {
+        return Arrays.stream(values())
+                .filter(apiVersion -> apiVersion.releaseType == type)
+                .sorted(Comparator.comparingInt(Enum::ordinal))
+                .sorted(Comparator.reverseOrder())
+                .reduce((a, b) -> b)
+                .orElse(ApiVersion.UNKNOWN);
+    }
+
     public enum ReleaseType {
-        SNAP,
-        PRE,
-        RC,
+        UNKNOWN,
+        SNAPSHOT,
+        PRE_RELEASE,
+        RELEASE_CANDIDATE,
         RELEASE,
     }
 }
