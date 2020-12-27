cc098c8c2a8100bb9dc9cf4daf7f4f2bb6ea44c0
Jeroen Verwimp <jeroen@verwimp.me>
2020-12-27 13:52:16 +0100

    Fix CLI for windows
    On windows the java -cp should be separated with semicolons not colons. This problem is solved by using the wildcard.
    
    + The jar tools where moved to its own folder

--- tools/scripts/decompile.sh
+++ tools/scripts/decompile.sh
@@ -4,11 +4,11 @@ mcVersion=$(cat ".loomversion")
 
 mapServerJar() {
     printf "Converting $mcVersion Minecraft mappings..."
-    java -cp tools/enigma-0.22.0.jar:tools/enigma-cli-0.22.0.jar:tools/guava-30.0-jre.jar cuchaz.enigma.command.Main convert-mappings proguard ".cache/$mcVersion/server.txt" tinyv2:obf:deobf ".cache/$mcVersion/server.tiny" || exit 1
+    java -cp "tools/jars/enigma/*" "cuchaz.enigma.command.Main" convert-mappings proguard ".cache/$mcVersion/server.txt" tinyv2:obf:deobf ".cache/$mcVersion/server.tiny" || exit 1
     printf " Done!\n"
 
     printf "Mapping $mcVersion Minecraft server jar...\n"
-    java -jar tools/tiny-remapper-0.3.1.72-fat.jar ".cache/$mcVersion/server.jar" ".cache/$mcVersion/server-deobf.jar" ".cache/$mcVersion/server.tiny" obf deobf --renameInvalidLocals || exit 1
+    java -jar "tools/jars/tiny-remapper-0.3.1.72-fat.jar" ".cache/$mcVersion/server.jar" ".cache/$mcVersion/server-deobf.jar" ".cache/$mcVersion/server.tiny" obf deobf --renameInvalidLocals || exit 1
 
     printf "Installing $mcVersion mapped Minecraft server jar in your local maven repo..."
     mvn install:install-file -Dfile=".cache/$mcVersion/server-deobf.jar" -DgroupId="org.loomdev" -DartifactId="minecraft-server" -Dversion="$mcVersion-SNAPSHOT" -Dpackaging="jar" > /dev/null
@@ -22,7 +22,7 @@ decompile() {
 
     printf "Decompiling $mcVersion mapped Minecraft source...\n"
     mkdir -p ".cache/$mcVersion/decompiled/"
-    java -jar "tools/fernflower.jar" -dgs=1 -hdc=0 -rbr=0 -asc=1 -udv=0 ".cache/$mcVersion/extracted/" ".cache/$mcVersion/decompiled/"
+    java -jar "tools/jars/fernflower.jar" -dgs=1 -hdc=0 -rbr=0 -asc=1 -udv=0 ".cache/$mcVersion/extracted/" ".cache/$mcVersion/decompiled/"
     printf "Done!\n"
 }
 
