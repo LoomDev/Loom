8b376be671e93be3ee491d8dbd811119beab5eb3
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-01-02 12:02:16 +0000

    Make requested changes

--- server/src/main/java/org/loomdev/loom/entity/player/PlayerImpl.java
+++ server/src/main/java/org/loomdev/loom/entity/player/PlayerImpl.java
@@ -1,9 +1,11 @@
 package org.loomdev.loom.entity.player;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.minecraft.Util;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.protocol.game.ClientboundGameEventPacket;
 import net.minecraft.network.protocol.game.ClientboundSetTitlesPacket;
 import net.minecraft.network.protocol.game.ClientboundSoundEntityPacket;
 import net.minecraft.network.protocol.game.ClientboundTabListPacket;
@@ -13,17 +15,25 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.level.GameType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.loomdev.api.Loom;
+import org.loomdev.api.bossbar.BossBar;
+import org.loomdev.api.entity.Entity;
 import org.loomdev.api.entity.EntityType;
 import org.loomdev.api.entity.player.Player;
 import org.loomdev.api.math.MathHelper;
 import org.loomdev.api.sound.Sound;
 import org.loomdev.api.util.GameMode;
+import org.loomdev.api.world.Location;
+import org.loomdev.api.world.Weather;
 import org.loomdev.loom.entity.LivingEntityImpl;
 import org.loomdev.loom.util.transformer.TextTransformer;
 
 import java.net.InetSocketAddress;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 
 public class PlayerImpl extends LivingEntityImpl implements Player {
 
--- server/src/main/java/org/loomdev/loom/util/ChatToLegacyConverter.java
+++ server/src/main/java/org/loomdev/loom/util/ChatToLegacyConverter.java
@@ -6,49 +6,67 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
-import org.checkerframework.checker.guieffect.qual.SafeType;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ChatUtil {
+public class ChatToLegacyConverter {
 
-    public static String toString(Component component) {
+    /**
+     * Converts a text component to a legacy string
+     * with colors replaced with their legacy codes
+     * (§ followed by a number or letter).
+     *
+     * @return The legacy string.
+     * @see https://minecraft.gamepedia.com/Formatting_codes
+     */
+    public static String toLegacy(Component component) {
         if (component == null) {
             return "";
         }
         StringBuilder result = new StringBuilder();
 
-        for (Component subComponent : components(component)) {
+        List<Component> components = stream(component).collect(Collectors.toList());
+        boolean hasFormatting = false;
+        for (Component subComponent : components) {
+            if (hasFormatting) {
+                // If the previous component was formatted, clear all formatting.
+                hasFormatting = false;
+                result.append(ChatFormatting.RESET);
+            }
             Style style = subComponent.getStyle();
             TextColor color = style.getColor();
 
             if (color != null) {
+                // Attempt to convert the colour to legacy
                 try {
                     result.append(ChatFormatting.valueOf(color.toString().toUpperCase()));
+                    hasFormatting = true;
                 } catch(IllegalArgumentException ignored) {
-                    result.append("§x");
-                    for (char character : color.toString().substring(1).toCharArray()) {
-                        result.append("§" + character);
-                    }
+                    // Legacy does not support RGB
                 }
             }
 
             if (style.isBold()) {
+                hasFormatting = true;
                 result.append(ChatFormatting.BOLD);
             }
             if (style.isItalic()) {
+                hasFormatting = true;
                 result.append(ChatFormatting.ITALIC);
             }
             if (style.isUnderlined()) {
+                hasFormatting = true;
                 result.append(ChatFormatting.UNDERLINE);
             }
             if (style.isStrikethrough()) {
+                hasFormatting = true;
                 result.append(ChatFormatting.STRIKETHROUGH);
             }
             if (style.isObfuscated()) {
+                hasFormatting = true;
                 result.append(ChatFormatting.OBFUSCATED);
             }
 
@@ -61,11 +79,11 @@ public class ChatUtil {
         return result.toString();
     }
 
-    public static Stream<Component> stream(Component component) {
-        return Streams.concat(Stream.of(component), component.getSiblings().stream().flatMap(ChatUtil::stream));
+    private static Stream<Component> stream(Component component) {
+        return Streams.concat(Stream.of(component), component.getSiblings().stream().flatMap(ChatToLegacyConverter::stream));
     }
 
-    public static List<Component> components(Component component) {
-        return stream(component).collect(Collectors.toList());
+    private ChatToLegacyConverter() {
+        throw new UnsupportedOperationException("ChatToLegacyConverter shouldn't be initialized.");
     }
 }
