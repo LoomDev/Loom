5ac6a7b0a9ac76fc85f57027c4a73030e64b4fe2
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-01-10 10:05:40 +0000

    Maven wrapper

--- tools/scripts/buildJar.sh
+++ tools/scripts/buildJar.sh
@@ -7,10 +7,10 @@ buildJar() {
     rootPath=$(pwd)
 
     cd "tools/Paperclip"
-    mvn clean package "-Dmcver=$mcVersion" "-Dpaperjar=$rootPath/server/target/loom-$mcVersion.jar" "-Dvanillajar=$rootPath/.cache/$mcVersion/server.jar" || exit 1
+    "$rootPath/mvnw" clean package "-Dmcver=$mcVersion" "-Dpaperjar=$rootPath/server/target/loom-$mcVersion.jar" "-Dvanillajar=$rootPath/.cache/$mcVersion/server.jar" || exit 1
     cp "assembly/target/paperclip-$mcVersion.jar" "$rootPath/loom-$mcVersion.jar"
     cd ../..
-    
+
     printf "Done!"
 }
 
--- tools/scripts/decompile.sh
+++ tools/scripts/decompile.sh
@@ -11,7 +11,7 @@ mapServerJar() {
     java -jar "tools/jars/tiny-remapper-0.3.1.72-fat.jar" ".cache/$mcVersion/server.jar" ".cache/$mcVersion/server-deobf.jar" ".cache/$mcVersion/server.tiny" obf deobf --renameInvalidLocals || exit 1
 
     printf "Installing $mcVersion mapped Minecraft server jar in your local maven repo..."
-    mvn install:install-file -Dfile=".cache/$mcVersion/server-deobf.jar" -DgroupId="org.loomdev" -DartifactId="minecraft-server" -Dversion="$mcVersion-SNAPSHOT" -Dpackaging="jar" > /dev/null
+    ./mvnw install:install-file -Dfile=".cache/$mcVersion/server-deobf.jar" -DgroupId="org.loomdev" -DartifactId="minecraft-server" -Dversion="$mcVersion-SNAPSHOT" -Dpackaging="jar" > /dev/null
     printf " Done!\n"
 }
 
